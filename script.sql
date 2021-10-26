1a79803d5ad49e80f86e99a132515fcdcab00b1a
MarcioRPereira <marciorpereira.ti@gmail.com>
2021-10-26 07:24:14 -0300

    Incremento no script do banco

--- script.sql
+++ script.sql
@@ -1,10 +1,36 @@
+drop database crudquarkus;
 create database crudquarkus;
-
 use crudquarkus;
+
+create table estado(
+	uf varchar(2) primary key,
+    descricao varchar(30)
+)ENGINE = innodb;
+
+create table cidade(
+	id int primary key auto_increment,
+    descricao varchar(200),
+    uf varchar(2),
+    foreign key (uf) references estado (uf) on delete cascade on update no action
+)ENGINE = innodb;
 
 create table pessoa(
 	id int primary key auto_increment,
     nome varchar(200),
     cpf varchar(14),
-    idade int
-);
+    idade int,
+	endereco varchar(300),
+    cidade_id int not null,
+	foreign key (cidade_id) references cidade (id) on delete cascade on update no action
+)ENGINE = innodb;
+
+insert into estado values ('SP', 'São Paulo');
+insert into estado values ('RJ', 'Rio de Janeiro');
+insert into estado values ('MG', 'Minas Gerais');
+insert into estado values ('ES', 'Espírito Santo');
+
+insert into cidade values (null, 'São Paulo', 'SP');
+insert into cidade values (null, 'São José do Rio Preto', 'SP');
+insert into cidade values (null, 'Rio de Janeiro', 'RJ');
+insert into cidade values (null, 'Belo Horizonte', 'MG');
+insert into cidade values (null, 'Vitória', 'ES');
